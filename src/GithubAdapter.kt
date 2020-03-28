41bb61308bc141a6de4076e5fc7e7356d717ca45
Brais Gabín <braisgabin@gmail.com>
2020-03-28 17:31:21 +0100

    Create GithubService

--- src/GithubAdapter.kt
+++ src/GithubAdapter.kt
@@ -1,17 +1,16 @@
 package com.braisgabin.seshat
 
 import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 import retrofit2.Response
-import retrofit2.http.Body
-import retrofit2.http.Headers
-import retrofit2.http.POST
-import retrofit2.http.Path
+import retrofit2.http.*
 
 interface GithubAdapter {
 
     @Headers("Accept: application/vnd.github.comfort-fade-preview+json")
     @POST("/repos/{owner}/{repo}/pulls/{pull_number}/comments")
     suspend fun addComment(
+        @Header("Authorization") authorization: String,
         @Path("owner") owner: String,
         @Path("repo") repo: String,
         @Path("pull_number") pullNumber: String,
@@ -19,19 +18,13 @@ interface GithubAdapter {
     ): Response<Unit>
 }
 
+@JsonClass(generateAdapter = true)
 data class PrCommentBody(
     val body: String,
     @Json(name = "commit_id") val commitId: String,
     val path: String,
     val side: String,
     val line: Int,
-    @Json(name = "start_side") val startSide: String,
-    @Json(name = "start_line") val startLine: Int
+    @Json(name = "start_side") val startSide: String?,
+    @Json(name = "start_line") val startLine: Int?
 )
-
-fun Side.toData(): String {
-    return when (this) {
-        Side.RIGHT -> "RIGHT"
-        Side.LEFT -> "LEFT"
-    }
-}
---
+++ src/GithubService.kt
@@ -0,0 +1,42 @@
+package com.braisgabin.seshat
+
+class GithubService(
+    private val githubAdapter: GithubAdapter,
+    private val oauthToken: String
+) {
+
+    suspend fun createComment(
+        owner: String,
+        repo: String,
+        pullNumber: String,
+        commitId: String,
+        comment: PullRequestComment
+    ): Boolean {
+        return githubAdapter.addComment(
+            authorization = "token $oauthToken",
+            owner = owner,
+            repo = repo,
+            pullNumber = pullNumber,
+            body = comment.toData(commitId)
+        ).isSuccessful
+    }
+}
+
+private fun PullRequestComment.toData(commitId: String): PrCommentBody {
+    return PrCommentBody(
+        body = body,
+        commitId = commitId,
+        path = path,
+        startSide = if (start == end) null else start.side.toData(),
+        startLine = if (start == end) null else start.line,
+        side = end.side.toData(),
+        line = end.line
+    )
+}
+
+private fun Side.toData(): String {
+    return when (this) {
+        Side.RIGHT -> "RIGHT"
+        Side.LEFT -> "LEFT"
+    }
+}
