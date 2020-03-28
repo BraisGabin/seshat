c879c57ba8e59783129dcd75a6028f6f295a89a3
Brais Gabín <braisgabin@gmail.com>
2020-03-28 17:59:15 +0100

    do the call

--- src/Application.kt
+++ src/Application.kt
@@ -12,6 +12,9 @@ import io.ktor.routing.post
 import io.ktor.routing.route
 import io.ktor.routing.routing
 import org.slf4j.event.Level
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -23,11 +26,26 @@ fun Application.module(testing: Boolean = false) {
         filter { call -> call.request.path().startsWith("/") }
     }
 
+    val githubAdapter = Retrofit.Builder()
+        .baseUrl("https://api.github.com")
+        .addConverterFactory(MoshiConverterFactory.create())
+        .build()
+        .create<GithubAdapter>()
+
+    val githubService = GithubService(githubAdapter, environment.config.property("ktor.github.token").getString())
+
     routing {
         route("github") {
-            post("{user}/{repository}/pulls/{pr}") {
+            post("{owner}/{repo}/pulls/{pull_number}/{commit_id}") {
                 val diff = parse(call.receiveChannel())
 
+                val owner: String = call.parameters["owner"]!!
+                val repo: String = call.parameters["repo"]!!
+                val pullNumber: String = call.parameters["pull_number"]!!
+                val commitId: String = call.parameters["commit_id"]!!
+                diff.forEach {
+                    githubService.createComment(owner, repo, pullNumber, commitId, it)
+                }
                 call.respondText(diff.toString(), contentType = ContentType.Text.Plain)
             }
         }
